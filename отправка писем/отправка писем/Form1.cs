56becb3ad379d9c67a99c44b8289f5ec39ab9b42
light73 <swetoshek@mail.ru>
2015-06-02 18:12:40 +0300

    отправка почты

---
+++ отправка писем/отправка писем/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Mail;
+using OpenPop.Pop3;
+using OpenPop.Mime;
+using System.IO;
+using System.Threading;
+
+namespace отправка_писем
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Visible = false;
+            button2.Enabled = false;
+        }
+
+        string fileName;
+
+      
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Авторизация av = new Авторизация();          
+            SmtpClient clientSMTP = new SmtpClient("smtp.mail.ru", 587);
+            MailAddress address = new MailAddress(textBox1.Text + "@mail.ru");
+            MailMessage message = new MailMessage();
+            message.To.Add(address);
+            message.From = new MailAddress("swetoshek@mail.ru");    //(av.info_from[0]);
+            message.IsBodyHtml = true;
+            message.Subject = textBox2.Text;
+            message.Body = textBox3.Text;
+            clientSMTP.Credentials = new System.Net.NetworkCredential("swetoshek@mail.ru", "230717");     //(av.info_from[0], av.info_from[1]);
+            clientSMTP.EnableSsl = true;
+            if (fileName != "")
+            {
+                Attachment[] att = new Attachment[1];
+                System.Net.Mail.Attachment attachment;                
+                attachment = new System.Net.Mail.Attachment(@fileName);
+                att[0] = attachment;
+                message.Attachments.Add(att[0]);                
+                clientSMTP.Send(message);
+            }
+            else
+            {
+                clientSMTP.Send(message);
+            }
+            MessageBox.Show("Ready !!!");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog chto_open = new OpenFileDialog();
+            if (chto_open.ShowDialog() == DialogResult.OK)
+            {                
+                fileName = chto_open.FileName;
+                pictureBox1.Visible = true;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "") button2.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Авторизация av = new Авторизация();
+            av.ShowDialog();
+        }
+    }
+}
---
+++ отправка писем/отправка писем/Авторизация.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace отправка_писем
+{
+    partial class Авторизация
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(131, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Войти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите логин";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "mail.ru"});
+            this.listBox1.Location = new System.Drawing.Point(212, 36);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(98, 17);
+            this.listBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(194, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Введите пароль";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 99);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(194, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Авторизация
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 200);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Авторизация";
+            this.Text = "Авторизация";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
